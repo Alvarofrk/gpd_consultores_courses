78a4005f3d6f9a9d0410f63cea0e89ef3927ab49
ALVARO CERNA <31244499+Alvarofrk@users.noreply.github.com>
2025-06-27 14:48:53 -0500

    fix cotización

--- core/templatetags/calendar_extras.py
+++ core/templatetags/calendar_extras.py
@@ -1,8 +1,16 @@
 from django import template
+import math
 
 register = template.Library()
 
 @register.filter
 def get_item(dictionary, key):
     """Filtro para obtener un elemento de un diccionario por clave"""
-    return dictionary.get(key) 
+    return dictionary.get(key)
+
+@register.filter
+def ceil1(value):
+    try:
+        return math.ceil(float(value) * 10) / 10
+    except (ValueError, TypeError):
+        return value 
